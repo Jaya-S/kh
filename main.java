baad027051f42fb2f6ae2a236f4ba0f37e5feee3
Jaya-S <jaivedgiri@gmail.com>
2017-07-05 22:04:39 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+# kh
+public class main{
+             
+             public static void main(String []args){
+               for(int i=0;i<5;i++){
+                 
+                 System.out.println("Helloworld");
+                 }
+                 }
+                 }
+
+               
+
+               
